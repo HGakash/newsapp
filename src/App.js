bfb2d8140e1c987d529eb5025f244cb18c977292
HGakash <hgakashgowda1234@gmail.com>
2024-12-12 17:22:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+     let c = 'jhon';
+    return (
+      <div>
+        hello my first class based component {c}
+      </div>
+    )
+  }
+}
